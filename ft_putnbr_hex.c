3b2e71ab9d03f2e781c194c7a049c1b8e83451fb
edifer21 <edifer5432@gmailcom>
2024-11-15 16:15:29 +0100

    falta normi

--- ft_putnbr_hex.c
+++ ft_putnbr_hex.c
@@ -10,7 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include <stdarg.h>
 #include <string.h>
 #include <stdlib.h>
@@ -19,29 +18,24 @@
 
 int	ft_putnbr_hex(unsigned long n)
 {
-    char hex_digits[] = "0123456789abcdef";
-    char buffer[20];
-    int i;
-    int count;
+	char	hex_digits[] = "0123456789abcdef";
+	char	buffer[20];
+	int		i;
+	int		count;
 
-    i =  0;
-    count = 0;
-
-    if (n == 0)
-    {
-        count += ft_putchar('0');
-        return (count);
-    }
-   
-    while (n > 0) 
-    {
-        buffer[i++] = hex_digits[n % 16];
-        n /= 16;
-    }
-
-    while (i-- > 0)
-
-        count += ft_putchar(buffer[i]);
-
-    return (count);
+	i = 0;
+	count = 0;
+	if (n == 0)
+	{
+		count += ft_putchar('0');
+		return (count);
+	}
+	while (n > 0)
+	{
+		buffer[i++] = hex_digits[n % 16];
+		n /= 16;
+	}
+	while (i-- > 0)
+		count += ft_putchar(buffer[i]);
+	return (count);
 }
